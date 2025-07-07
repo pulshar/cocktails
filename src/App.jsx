c8049193dca4d10e4c62bc709bc0e17770a11cf9
Alvaro Quevedo <alvaroq@gmail.com>
2025-07-07 19:21:12 +0200

    Hero and cocktails sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import gsap from 'gsap';
-import { ScrollTrigger, SplitText} from "gsap/all";
-import Navbar from '@/components/Navbar';
+import Cocktails from "@/components/Cocktails";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default function App() {
-  return <main><Navbar/></main>;
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Cocktails />
+    </main>
+  );
 }
