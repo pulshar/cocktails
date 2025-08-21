ac0765691e7f0e8e537a701ced891e09987dcec4
Alvaro Quevedo <alvaroq@gmail.com>
2025-08-21 14:46:42 +0200

    New sections added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { ReactLenis } from 'lenis/react'
+import { ReactLenis } from "lenis/react";
 
 import About from "@/components/About";
+import Art from "@/components/Art";
 import Cocktails from "@/components/Cocktails";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Menu from "@/components/Menu";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 
@@ -12,13 +14,15 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 export default function App() {
   return (
     <>
-    <ReactLenis root options={{ lerp: 0.12 }} />
-    <main>
-      <Navbar />
-      <Hero />
-      <Cocktails />
-      <About />
-    </main>
+      <ReactLenis root options={{ lerp: 0.12 }} />
+      <main>
+        <Navbar />
+        <Hero />
+        <Cocktails />
+        <About />
+        <Art />
+        <Menu />
+      </main>
     </>
   );
 }
